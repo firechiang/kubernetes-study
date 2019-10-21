874b3bb17b977e374c556d68ac6663ef0f7a0728
chiangfire <chiang-fire@outlook.com>
2019-10-21 20:43:38 +0800

    feat: add k8s-deploy-springcloud-service

---
+++ k8s-deploy/k8s-deploy-springcloud-service/src/main/java/com/firecode/k8s/deploy/springcloud/service/controller/TestController.java
@@ -0,0 +1,23 @@
+package com.firecode.k8s.deploy.springcloud.service.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	
+	@Autowired
+	private Environment environment;
+	
+	/**
+	 * 获取服务名称
+	 * @return
+	 */
+	@GetMapping
+	public String getServiceName() {
+		
+		return String.join("-", environment.getProperty("spring.application.name"),String.valueOf(System.currentTimeMillis()));
+	}
+}
