eff2e2dcfea36b9cf9465a3a2834085aaaed9613
chiangfire <chiang-fire@outlook.com>
2019-10-28 21:14:44 +0800

    feat: add jenkins

---
+++ jenkins/README.md
@@ -0,0 +1,8 @@
+#### 下载安装包
+```bash
+$ wget -P /home/jenkins http://mirrors.jenkins.io/war-stable/latest/jenkins.war
+```
+#### 启动Jenkins
+```bash
+$ java -jar /home/jenkins/jenkins.war --httpPort=8099
+```
---
+++ jenkins/src/main/java/com/firecode/kubernetes_study/jenkins/App.java
@@ -0,0 +1,13 @@
+package com.firecode.kubernetes_study.jenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
